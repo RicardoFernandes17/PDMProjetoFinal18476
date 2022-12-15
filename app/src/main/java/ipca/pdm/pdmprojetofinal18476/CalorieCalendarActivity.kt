c37ea613e3e5749d9f913012e0395b8907e1078a
RicardoFernandes17 <a18476@alunos.ipca.pt>
2022-12-15 19:59:00 +0000

    New Activity Created

--- app/src/main/java/ipca/pdm/pdmprojetofinal18476/CalorieCalendarActivity.kt
+++ app/src/main/java/ipca/pdm/pdmprojetofinal18476/CalorieCalendarActivity.kt
@@ -3,11 +3,9 @@ package ipca.pdm.pdmprojetofinal18476
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CalorieCalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_calorie_calendar)
     }
 }
